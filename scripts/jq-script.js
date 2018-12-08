b520699d3e293e39ee79fb3f7245a5fa5dc85c12
BBvLucky <35274237+BBvLucky@users.noreply.github.com>
2018-12-08 20:42:25 +0300

    New portfolio is ready!

---
+++ scripts/jq-script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+    //Выпадающее меню
+    $('.header-nav-menu-btn').on('click', function () {
+        $('.header-nav-link').toggleClass('toggle');
+        $('.middle').toggleClass('close');
+    })
+    $('.header-nav-link').on('click', function () {
+        $(this).addClass('toggle')
+    })
+
+    //Анимация "якорей"
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        let scroll = $(this).attr('href'),
+            top = $(scroll).offset().top;
+
+        $('body, html').animate({
+            scrollTop: top
+        }, 500)
+    })
+
+    //"Показать/скрыть" всё
+    $('.portfolio-show-more').on('click', function () {
+        $('.portfolio').toggleClass('portfolio-xpand');
+        $(this).toggleClass('portfolio-show-more-hide');
+        if ($(this).hasClass('portfolio-show-more-hide')) {
+            $(this).html('<p>скрыть</p>');
+        } else {
+            $(this).html('<p>показать все</p>')
+        }
+    })
+
+    //Прогресс бар
+
+    function animProgress(persent, $element) {
+        $element.find('div').animate({
+            width: persent + '%'
+        }, 500).html(persent + '%');
+    }
+
+    $('#skills').offset().top;
+    
+
+    $('.skills-progressbar').each(function (i) {
+        animProgress($('.skills-progressbar-bar').eq(i).text(), $(this))
+    })
+})
